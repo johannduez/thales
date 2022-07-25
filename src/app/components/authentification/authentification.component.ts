f53ef192017d24d1df2e15c240d910915b47c4c1
EA <williamea94700@hotmail.fr>
2022-07-25 14:49:41 +0200

    Commit authetification

--- src/app/components/authentification/authentification.component.ts
+++ src/app/components/authentification/authentification.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Commande } from 'src/app/classes/commande';
 import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Client } from 'src/app/classes/client';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-authentification',
@@ -9,25 +12,51 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./authentification.component.css']
 })
 export class AuthentificationComponent implements OnInit {
+  registerForm: FormGroup;
+  client: Client = new Client();
+  clientTest: Client = new Client();
+  erreurFlag: boolean = false;
 
-  constructor(private router:Router,private auth:AuthService) { }
+  constructor(private router:Router,private auth:AuthService, private formBuilder: FormBuilder, private service: ClientService) { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      id: new FormControl(this.client.id, [Validators.required]),
+      password: new FormControl(this.client.password, [Validators.required])
+
+  });
   }
-f(){
-  let commande:Commande=new Commande();
-  commande.client.id=1;
-  commande.client.adresse="greasque";
-   commande.client.nom="duez";
-  commande.client.password="1";
-  commande.client.prenom="johann";
-   commande.prixTotal=0;
-   let str:string=JSON.stringify(commande);
-  sessionStorage.setItem("commande",str);
-  console.log(commande);
-  //window.location.reload();
-  this.auth.isLoggedIn=true;
-  this.auth.nom=commande.client.nom;
-  this.auth.prenom=commande.client.prenom;
-}
+
+  get id() { return this.registerForm.get('id'); }
+  get password() { return this.registerForm.get('password'); }
+
+  onSubmit() {
+
+    this.service.findbyIdAndPassword2(this.client).subscribe(
+      response => {
+        console.log(response);
+
+        if(response != null){
+          this.client = response;
+    
+          let commande: Commande = new Commande();
+          commande.client = this.client;
+          sessionStorage.setItem("commande", JSON.stringify(commande));
+    
+          this.auth.isLoggedIn=true;
+          this.auth.nom=commande.client.nom;
+          this.auth.prenom=commande.client.prenom;
+          
+          this.router.navigate(['/accueil']);
+        } else {
+          this.erreurFlag = true;
+        } 
+
+      },
+      err => {
+          console.log("***** Erreur *****");
+      }
+    );
+  }
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Commande } from '../classes/commande';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn: boolean = false;
+  isLoggedIn: boolean;
   nom:string;
   prenom:string;
 
-  constructor() { }
+  constructor() { 
+    if(sessionStorage.getItem("commande") != null){
+      this.isLoggedIn = true;
+      this.nom = JSON.parse(sessionStorage.getItem("commande")).client.nom;
+      this.prenom = JSON.parse(sessionStorage.getItem("commande")).client.prenom;
+    } else {
+      this.isLoggedIn = false;
+    }
+  }
 }
