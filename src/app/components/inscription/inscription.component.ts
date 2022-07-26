e2cb844a967e9f00f683357f343dbf4ba20a3d8a
EA <williamea94700@hotmail.fr>
2022-07-26 12:42:52 +0200

    Commit Confirmation mot de passe inscription

--- src/app/components/inscription/inscription.component.ts
+++ src/app/components/inscription/inscription.component.ts
@@ -15,6 +15,8 @@ export class InscriptionComponent implements OnInit {
   client: Client = new Client();
   erreurFlag: boolean = false;
   erreurIdFlag: boolean = false;
+  erreurPasswordCFlag: boolean = false;
+  passwordConfirmation: string;
 
   constructor(private router: Router, private service: ClientService) { }
 
@@ -22,6 +24,7 @@ export class InscriptionComponent implements OnInit {
     this.registerForm = new FormGroup({
       id: new FormControl(this.client.id, [Validators.required]),
       password: new FormControl(this.client.password, [Validators.required]),
+      passwordConfirmation: new FormControl(this.passwordConfirmation, [Validators.required]),
       nom: new FormControl(this.client.nom, [Validators.required]),
       prenom: new FormControl(this.client.prenom, [Validators.required]),
       adresse: new FormControl(this.client.adresse, [Validators.required]),
@@ -31,17 +34,30 @@ export class InscriptionComponent implements OnInit {
 
   get id() { return this.registerForm.get('id'); }
   get password() { return this.registerForm.get('password'); }
+  get passwordC() { return this.registerForm.get('passwordConfirmation'); }
   get nom() { return this.registerForm.get('nom'); }
   get prenom() { return this.registerForm.get('prenom'); }
   get adresse() { return this.registerForm.get('adresse'); }
 
   onSubmit(){
+    if(this.client.password != this.passwordConfirmation)
+      this.erreurPasswordCFlag = true;
+    else 
+      this.erreurPasswordCFlag = false;
+    
+
+    if(this.client.id>0)
+      this.erreurIdFlag = false;
+    else 
+      this.erreurIdFlag = true;
+
     this.service.findbyId(this.client.id).subscribe(
       response => {
         this.erreurFlag = true;
       },
       err => {
-        if(this.client.id>0){
+        this.erreurFlag = false;
+        if(!this.erreurFlag && !this.erreurIdFlag && !this.erreurPasswordCFlag){
           this.service.create(this.client).subscribe(
             response => {
               this.router.navigate(['/authentification']);
@@ -49,8 +65,6 @@ export class InscriptionComponent implements OnInit {
             err => {
               this.erreurFlag = true;
             }); 
-        } else {
-          this.erreurIdFlag = true;
         }
 
       });
